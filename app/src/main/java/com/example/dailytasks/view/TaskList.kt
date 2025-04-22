849725ae80507aa89044cb3b62cdd7aa2dddd188
rdgferino <raoniferino@gmail.com>
2025-04-22 02:02:04 +0100

    NavHost, NavController

---
+++ app/src/main/java/com/example/dailytasks/view/TaskList.kt
@@ -0,0 +1,13 @@
+package com.example.dailytasks.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun TaskList(
+    navController: NavController
+){
+Text("Eu vou conseguir")
+
+}
